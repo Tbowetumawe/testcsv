45fb405d4f7e63950813e8e80188881ece75a7cd
tsashua <tsashua.bowetumawe.sio@gmail.com>
2020-08-02 22:09:53 +0200

    first commit

---
+++ src/Command/cmd.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputAwareInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\DependencyInjection\Loader\ProtectedPhpFileLoader;
+
+class CsvCommande extends Command{
+
+    private $em;
+    protected static $defaultName='testDnd:affiche';
+
+    protected function configure()
+    {
+        $this
+            ->setName('csv:affiche')
+            ->setDescription('afficher la grille')
+            ->addArgument('lien', InputArgument::REQUIRED, 'link');
+    }
+    
+
+    /*private $csvOptions = array(
+        'finder_in' => 'public',
+        'finder_name' => 'products.csv',
+        
+    );*/
+
+    protected function execute(InputInterface $input, OutputInterface $output){
+        
+        $lienCsv = $input->getArgument('lien');
+        $open = fopen("$lienCsv", "r");
+        if (fgetcsv($open)){   
+        }
+        
+        fclose($open);
+        return Command::SUCCESS;
+    }
+
+}
